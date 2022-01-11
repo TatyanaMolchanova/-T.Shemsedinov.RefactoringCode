0df60e009a94778fe91a5251c6ad38b39ef0d174
Molchanova Tatyana <mtuspeh@gmail.com>
2022-01-11 06:55:14 +0200

    difference task solved

--- differenceRefactored.js
+++ differenceRefactored.js
@@ -3,21 +3,26 @@
 const findDifference = (obj1, obj2) => {
     if (JSON.stringify(obj1) === JSON.stringify(obj2)) return false;
     
+    const copyObj1 = JSON.parse(JSON.stringify(obj1));
+    const keysObj1 = Object.keys(obj1).sort();
+    const keysObj2 = Object.keys(obj2).sort();
 
+    for (let i = 0; i < keysObj1.length; i++) {
+       
+        if (!keysObj1.includes(keysObj2[i])) {
+            return { [keysObj1[i]]: obj1[keysObj1[i]] };
+        }
+    }
+
+    for (const key in obj2) {
+        
+        if (obj1[key] === obj2[key]) {
+            delete copyObj1[key];
+        }
+
+        return copyObj1;
+    }
 }
 
-
-// const diff =DIFF= function diff(object_1,object_2){
-//     for (attribute_name in object_1) {
-//     object_1[attribute_name] = object_1[attribute_name]
-//     }{}
-//     if (object_1 in object_2) return false
-//     for (attribute_name in object_2) {
-//     object_1[attribute_name] = object_2[attribute_name]
-//     delete object_1[attribute_name]
-//     }{}
-//     return object_1
-// }
-
-// const result = diff({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
-// console.log(result);
+const result = findDifference({ a: 'uno', b: 'due' }, { a: 'uno', c: 'tre' });
+console.log(result);
